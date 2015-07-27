cad9bb29f4271130d99ca6577dad6b8340b04941
rin0801 <okuyama.chisa@mwed.co.jp>
2015-07-27 11:28:57 +0900

    演習7.6.2

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -29,6 +29,16 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+        it { should have_content("can't be blank") }
+        it { should have_content('is invalid') }
+        it { should have_content('is too short') }
+      end
     end
 
     describe "with valid information" do
